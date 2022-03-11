7e6128ba04043b926ed830820cf5f094eea68ecb
farzanemaleki <farzane.maleki.fm@gmail.com>
2022-03-11 12:23:58 +0330

    initialize a new project for fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MovieList from './Components/MovieList';
 import './App.css';
 
 function App() {
+  
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
+  const[movies , setMovies] = useState(dummyMovies);
+
+  // ravesh aval
+  // const fetchMoviesHandler = event =>{
+  //   fetch('https://swapi.dev/api/films/')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     const transformeMovies = data.results.map(result => 
+  //       {return{
+  //         id: result.episode_id,
+  //         title: result.title,
+  //         openingText: result.opening_crawl,
+  //         releaseDate: result.release_date
+  //       }}
+  //     );
+  //     setMovies(transformeMovies);
+  //   })
+  // }
+
+  //ravesh dovom
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+
+    const transformeMovies = data.results.map(result => {
+      return{
+        id: result.episode_id,
+        title: result.title,
+        openingText: result.opening_crawl,
+        releaseDate: result.release_date
+      }}
+    );
+    setMovies(transformeMovies);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieList movies={movies} />
+      </section>
+    </>
   );
 }
 
---
+++ src/Components/MovieList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Movie from "./Movie";
+
+import styles from './MovieList.module.css';
+
+const MovieList = props =>{
+    return(
+        <ul className={styles['movies-list']}>
+        {props.movies.map((movie) => (
+            <Movie
+            key={movie.id}
+            title={movie.title}
+            releaseDate={movie.releaseDate}
+            openingText={movie.openingText}
+            />
+        ))}
+        </ul>
+    );
+}
+
+export default MovieList;
